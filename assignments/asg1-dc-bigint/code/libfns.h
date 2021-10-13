695cdb2e21b7b095eeb84957427a731278b2062b
Andrew Scott Lim <ansclim@unix2.lt.ucsc.edu>
2021-10-12 19:51:38 -0700

    Initial commit

---
+++ assignments/asg1-dc-bigint/code/libfns.h
@@ -0,0 +1,13 @@
+// $Id: libfns.h,v 1.2 2021-10-06 17:23:17-07 - - $
+
+// Library functions not members of any class.
+
+#ifndef __LIBFNS_H__
+#define __LIBFNS_H__
+
+#include "bigint.h"
+
+bigint pow (const bigint& base, const bigint& exponent);
+
+#endif
+
