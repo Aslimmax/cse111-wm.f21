b8ee19af17fb26c62af2f586e36091d32b8a07a7
Andrew Scott Lim <ansclim@unix2.lt.ucsc.edu>
2021-10-12 20:16:32 -0700

    Formatting and replacing private members

--- assignments/asg1-dc-bigint/code/ubigint.h
+++ assignments/asg1-dc-bigint/code/ubigint.h
@@ -14,24 +14,24 @@ using namespace std;
 class ubigint {
    friend ostream& operator<< (ostream&, const ubigint&);
    private:
-      using unumber = unsigned long;
-      unumber uvalue {};
+        using ubigvalue_t = vector<uint8_t>
+        ubigvalue_t ubig_value;
    public:
-      void multiply_by_2();
-      void divide_by_2();
+        void multiply_by_2();
+        void divide_by_2();
 
-      ubigint() = default; // Need default ctor as well.
-      ubigint (unsigned long);
-      ubigint (const string&);
+        ubigint() = default; // Need default ctor as well.
+        ubigint (unsigned long);
+        ubigint (const string&);
 
-      ubigint operator+ (const ubigint&) const;
-      ubigint operator- (const ubigint&) const;
-      ubigint operator* (const ubigint&) const;
-      ubigint operator/ (const ubigint&) const;
-      ubigint operator% (const ubigint&) const;
+        ubigint operator+ (const ubigint&) const;
+        ubigint operator- (const ubigint&) const;
+        ubigint operator* (const ubigint&) const;
+        ubigint operator/ (const ubigint&) const;
+        ubigint operator% (const ubigint&) const;
 
-      bool operator== (const ubigint&) const;
-      bool operator<  (const ubigint&) const;
+        bool operator== (const ubigint&) const;
+        bool operator<  (const ubigint&) const;
 };
 
 #endif
